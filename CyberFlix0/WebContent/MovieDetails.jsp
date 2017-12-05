9215af7fd2f694f38596c3714657e20e5b03f73a
Damien <puppetmaster123x@gmail.com>
2017-12-05 00:44:46 -0600

    sdasdasfgbfgh Add actors to fucking detail servlet`

--- CyberFlix0/WebContent/MovieDetails.jsp
+++ CyberFlix0/WebContent/MovieDetails.jsp
@@ -4,7 +4,7 @@
 <%@ taglib prefix="sql" uri="http://java.sun.com/jsp/jstl/sql" %>  
 <%@page import="java.util.List, java.util.ArrayList, edu.txstate.internet.cyberflix.data.film.*, 
 edu.txstate.internet.cyberflix.data.db.FilmDAO, java.sql.*, 
-edu.txstate.internet.cyberflix.data.db.DAO "%>   
+edu.txstate.internet.cyberflix.data.db.DAO, edu.txstate.internet.cyberflix.data.actor.*"%>   
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -32,7 +32,7 @@ edu.txstate.internet.cyberflix.data.db.DAO "%>
 <div id="detailinfo" class="w3-display-topmiddle" style="margin-top:10%;">
     <div class="w3-card-4 w3-center" style="background-color:white">
     <h2 class="w3-container w3-black"><i>${film.title}</i></h2>
-    <img class="w3-round" src="Images/movie2.jpeg">
+    <img class="w3-round" src="images/2.jpg">
     <hr>
     <p id="description" class="lead" style="margin:5%"]>${film.description}</p>
     <hr>
@@ -40,7 +40,7 @@ edu.txstate.internet.cyberflix.data.db.DAO "%>
       <li class="w3-left-align"><b>Rating: </b>${film.rating}</li>
       <li class="w3-left-align"><b>Release Year: </b>${film.releaseYear}</li>
       <li class="w3-left-align"><b>Runtime: </b>${film.length} minutes</li>
-      <li class="w3-left-align"><b>Cast: </b>${film.actors}</li>
+      <li class="w3-left-align"><b>Cast: </b>${actors}</li>
     </ul>
   </div>
 </div>
--- CyberFlix0/src/edu/txstate/internet/cyberflix/CyberFlixMovieDetailServlet.java
+++ CyberFlix0/src/edu/txstate/internet/cyberflix/CyberFlixMovieDetailServlet.java
@@ -10,10 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.PrintWriter;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.txstate.internet.cyberflix.data.film.*;
+import edu.txstate.internet.cyberflix.data.film.Film.FilmRating;
+import edu.txstate.internet.cyberflix.data.helper.FilmFactory;
 import edu.txstate.internet.cyberflix.utils.HTMLTags;
 import edu.txstate.internet.cyberflix.data.db.*;
+import edu.txstate.internet.cyberflix.data.actor.*;
 
 /**
  * Servlet implementation class CyberFlixMovieDetailServlet
@@ -35,25 +40,89 @@ public class CyberFlixMovieDetailServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		FilmCatalog catalog    = FilmCatalog.getInstance();
+//		FilmCatalog catalog    = FilmCatalog.getInstance();
+//		
+//		String      filmIDStr  = request.getParameter("film_id");
+//		int         filmID     = Integer.valueOf(filmIDStr);
+//		Film        film       = catalog.get(filmID);
+//		
+//		Connection conn = null;
+//		conn = DAO.getDBConnection();
+//		FilmDAO filmDAO = new FilmDAO();
+//		Film detailFilm = null;
+//
+//		detailFilm = filmDAO.getFilmDetail(film);
+//		
+//		film.setActorsString(film.getActors());
+//		request.setAttribute("film", detailFilm);
+//		
+//
+//		request.getRequestDispatcher("MovieDetails.jsp").forward(request, response);
+//	
 		
-		String      filmIDStr  = (String)request.getParameter("film_id");
-		int         filmID     = Integer.valueOf(filmIDStr);
-		Film        film       = catalog.get(filmID);
-		
-		Connection conn = null;
-		conn = DAO.getDBConnection();
-		FilmDAO filmDAO = new FilmDAO();
-		Film detailFilm = null;
-
-		detailFilm = filmDAO.getFilmDetail(film);
-		
-		film.setActorsString(film.getActors());
-		request.setAttribute("film", detailFilm);
-		
-
-		request.getRequestDispatcher("MovieDetails.jsp").forward(request, response);
-		
+		try {
+			ActorDAO actorDAO = new ActorDAO();
+			Connection conn = DAO.getDBConnection();
+			Statement statement = conn.createStatement();
+			String      filmIDStr  = (String)request.getParameter("film_id");
+			int         filmID     = Integer.valueOf(filmIDStr);
+			String statementString = "SELECT film.film_id, film.title, film.description, film.length, film.rating, film.release_year FROM film WHERE film.film_id = " + filmIDStr;
+			ResultSet result = statement.executeQuery(statementString);
+			
+			int length;
+			String title, description, releaseYear, rating;
+			FilmRating rawRating;
+			while (result.next())
+			{
+			filmID = result.getInt("film.film_id");
+			title = result.getString("film.title");
+			description = result.getString("film.description");
+			releaseYear = result.getString("film.release_year");
+			length = result.getInt("film.length");
+			rating = result.getString("film.rating");
+			
+			rawRating = FilmFactory.convert(rating);
+			
+			Connection actorConn = DAO.getDBConnection();
+			Statement actorStatement = actorConn.createStatement();
+			List<Actor> actorList = new ArrayList <Actor>();
+			String actorStatementString = "SELECT actor_id, first_name, last_name FROM actor WHERE actor_id IN (SELECT actor_id FROM film_actor WHERE film_id =" + filmID + ")";
+			ResultSet actorResult = actorStatement.executeQuery(actorStatementString);
+			
+			int c = 0;
+			while(actorResult.next())
+			{
+				String firstName, lastName;
+				int actorID;
+				
+				firstName = actorResult.getString("actor.first_name");
+				lastName = actorResult.getString("actor.last_name");
+				actorID = actorResult.getInt("actor.actor_id");
+				
+				Actor actor = new Actor(actorID, firstName, lastName);
+				actorList.add(c, actor);
+				c++;
+			}
+			
+			String actorsString = "";
+			for(int i = 0; i < actorList.size(); i++)
+			{
+				if(i < actorList.size()-1)
+				{
+					actorsString = actorsString + actorList.get(i).getFirstName() + " " + actorList.get(i).getLastName() + ", ";				
+				}
+				else
+					actorsString = actorsString + actorList.get(i).getFirstName() + " " + actorList.get(i).getLastName();
+			}
+			
+			Film film = new Film(filmID, title, description, releaseYear, length, rawRating, actorList);
+			request.setAttribute("film", film);
+			request.setAttribute("actors", actorsString);
+			}
+			request.getRequestDispatcher("MovieDetails.jsp").forward(request, response);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
 	}
 
 	/**
